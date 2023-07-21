2bfd179f1db6ff92ce73e94d67fe0ea05080e5e1
benguimo <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>
2023-07-21 17:05:47 +0000

    Ticket-9 Done

--- src/components/topics/Football.jsx
+++ src/components/topics/Football.jsx
@@ -1,19 +1,18 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
-import { getTopics } from "../../api";
-import ArticlesGrid from "./ArticlesGrid";
-import goback from "../images/goback.png";
+import { getTopics } from "../../../api.js";
+import ArticlesGrid from "../../components/ArticlesGrid";
 import { useEffect, useState } from "react";
-import { getArticles } from "../../api";
-import "./Articles.css"
+import { getArticles } from "../../../api";
 
 
-function Articles() {
+function Football() {
   const [articles, setArticles] = useState([]);
   const [topics, setTopics] = useState([]);
   const [search, setSearch] = useState("");
   const [loading, setLoading] = useState(true);
   const[error, setError] = useState(false);
+
 
 
   useEffect(() => {
@@ -26,8 +25,9 @@ function Articles() {
           setError(false)   
         });
         getTopics().then((res) => {
-          setTopics(res);
-        }) .catch((err) => {
+          setTopics(res[1].slug);
+        
+        }).catch((err) => {
           setLoading(false)
           setError(true)
       })
@@ -36,7 +36,7 @@ function Articles() {
 
     useEffect(() => {
       getTopics().then((res) => {
-        setTopics(res);
+        setTopics(res[1].slug);
       });
     }, []);
   
@@ -53,15 +53,6 @@ function Articles() {
     })
     }
   
-  
-    function goBack() {
-      getArticles().then((res) => {
-        const newList = res.filter((article) => article.title.toLowerCase().includes(search.toLowerCase()));
-        setArticles(newList);
-      });
-    }
-
-
     if (loading)  return (<h2 className='message'>Loading</h2>) 
     if(error) return (<h2 className='message'>Oops! Something wennt wrong...</h2>)
 
@@ -71,16 +62,16 @@ function Articles() {
       <h2>Menu</h2>
       <ul className="topics">
              <NavLink to="/coding"><button onClick={chooseTopic}>Coding</button></NavLink> 
-             <NavLink to="/football"><button onClick={chooseTopic}>Football</button></NavLink> 
              <NavLink to="/cooking"><button onClick={chooseTopic}>Cooking</button></NavLink> 
-        <button onClick={goBack}>All</button>
+             <NavLink to="/articles"><button>All</button></NavLink> 
       </ul>
             <p><br />Ticket-9.</p>
     </aside>
     <main>
-      <h1>List of Articles</h1>
+      <h1>Football articles</h1>
       <ul className="list">
         {articles.map((article) => {
+          if (article.topic === topics)
           return <ArticlesGrid article={article} key={article.article_id} />;
         })}
       </ul>
@@ -89,4 +80,4 @@ function Articles() {
   )
 }
 
-export default Articles
+export default Football;
